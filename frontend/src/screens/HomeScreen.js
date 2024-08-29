4e2e95b3daa3cb099ec70e5ef556a16481072dfd
Ameynuddin <md.ameynuddin@gmail.com>
2024-08-29 13:06:34 +0800

    removed reducer logger

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,5 @@
 import { useEffect, useReducer, useState } from 'react';
 import axios from 'axios';
-import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
@@ -8,6 +7,17 @@ import { Helmet } from 'react-helmet-async';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 // import data from '../data';
+
+
+const logger = (reducer) => {
+    return (state, action) => {
+        console.log('Previous State:', state);
+        console.log('Action:', action);
+        const newState = reducer(state, action);
+        console.log('Next State:', newState);
+        return newState;
+    };
+}
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -43,6 +53,7 @@ function HomeScreen() {
         };
         fetchData();
     }, []);
+
     return (
         <div>
             <Helmet>
