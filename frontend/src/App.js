10b5c113a60fa1b2b09c85d20789e4fee0cf8c25
Ameynuddin <md.ameynuddin@gmail.com>
2024-09-01 13:36:38 +0800

    moved navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,10 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Navbar from './components/NavBar';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-import Navbar from 'react-bootstrap/Navbar';
-import Badge from 'react-bootstrap/Badge';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
-import { LinkContainer } from 'react-router-bootstrap';
-import { useContext } from 'react';
-import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
@@ -25,78 +19,18 @@ import AllProdScreen from './screens/AllProdScreen';
 import OrderHistoryScreen from './screens/OrderHistoryScreen';
 
 
-function App() {
-  const { state, dispatch: ctxDispatch } = useContext(Store);
-  const { cart, userInfo } = state;
 
-  const signoutHandler = () => {
-    ctxDispatch({ type: "USER_SIGNOUT" });
-    localStorage.removeItem("userInfo");
-    localStorage.removeItem("shippingAddress");
-    localStorage.removeItem("paymentMethod");
-    window.location.href = "/signin";
-  };
+function App() {
+
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
         <ToastContainer position="bottom-center" limit={1} />
+
         <header>
-          <Navbar bg="primary" data-bs-theme="dark">
-            <Container>
-              <LinkContainer to="/">
-              <Navbar.Brand>
-                  <img
-                    src="/silhouette.png" //logo from hhttps://www.flaticon.com/free-icon/silhouette_12768167?term=fashion+logo&page=1&position=11&origin=search&related_id=12768167
-                    alt="Hackashop Logo"
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                  />{' '}
-                  hackashop
-                </Navbar.Brand>
-              </LinkContainer>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Link to="/categories/all-products" className="nav-link">
-                  Products
-                </Link>
-                <Link to="/cart" className="nav-link">
-                  Cart
-                  {cart.cartItems.length > 0 && (
-                    <Badge pill bg="danger">
-                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
-                    </Badge>
-                  )}
-                </Link>
-                {userInfo ? (
-                  <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
-                    <LinkContainer to="/profile">
-                      <NavDropdown.Item>User Profile</NavDropdown.Item>
-                    </LinkContainer>
-                    <LinkContainer to="/orderhistory">
-                      <NavDropdown.Item>Order History</NavDropdown.Item>
-                    </LinkContainer>
-                    <NavDropdown.Divider />
-                    <Link
-                      className="dropdown-item"
-                      to="#signout"
-                      onClick={signoutHandler}
-                      >
-                      Sign Out
-                    </Link>
-                  </NavDropdown>
-                ) : (
-                  <Link className="nav-link" to="/signin">
-                    Sign In
-                  </Link>
-                )}
-              </Nav>
-              </Navbar.Collapse>
-              
-            </Container>
-          </Navbar>
+          <Navbar />
         </header>
+
         <main>
           <Container className="mt-3">
             <Routes>
@@ -117,10 +51,11 @@ function App() {
                 <Route path=":category" element={<CategoryScreen />} />
                 <Route path="all-products" element={<AllProdScreen />} />
               </Route>
-              <Route path="/orderhistory" element={<OrderHistoryScreen />}/>
+              <Route path="/orderhistory" element={<OrderHistoryScreen />} />
             </Routes>
           </Container>
         </main>
+        
         <footer>
           <div className="text-center">All rights reserved</div>
         </footer>
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,83 @@
+import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Badge from 'react-bootstrap/Badge';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useContext } from 'react';
+import { Store } from '../Store';
+
+function NavBar() {
+    const { state, dispatch: ctxDispatch } = useContext(Store);
+    const { cart, userInfo } = state;
+
+    const signoutHandler = () => {
+        ctxDispatch({ type: "USER_SIGNOUT" });
+        localStorage.removeItem("userInfo");
+        localStorage.removeItem("shippingAddress");
+        localStorage.removeItem("paymentMethod");
+        window.location.href = "/signin";
+    };
+
+    return (
+        <Navbar bg="primary" data-bs-theme="dark">
+            <Container>
+                <LinkContainer to="/">
+                    <Navbar.Brand>
+                        <img
+                            src="/wardrobe.png"
+                            alt="Hackashop Logo"
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />{' '}
+                        Hackashop
+                    </Navbar.Brand>
+                </LinkContainer>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Link to="/categories/all-products" className="nav-link">
+                            Products
+                        </Link>
+                        <Link to="/cart" className="nav-link">
+                            Cart
+                            {cart.cartItems.length > 0 && (
+                                <Badge pill bg="danger">
+                                    {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                                </Badge>
+                            )}
+                        </Link>
+                        {userInfo ? (
+                            <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+                                <LinkContainer to="/profile">
+                                    <NavDropdown.Item>User Profile</NavDropdown.Item>
+                                </LinkContainer>
+                                <LinkContainer to="/orderhistory">
+                                    <NavDropdown.Item>Order History</NavDropdown.Item>
+                                </LinkContainer>
+                                <NavDropdown.Divider />
+                                <Link
+                                    className="dropdown-item"
+                                    to="#signout"
+                                    onClick={signoutHandler}
+                                >
+                                    Sign Out
+                                </Link>
+                            </NavDropdown>
+                        ) : (
+                            <Link className="nav-link" to="/signin">
+                                Sign In
+                            </Link>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+
+            </Container>
+        </Navbar>
+    );
+}
+
+
+export default NavBar;
