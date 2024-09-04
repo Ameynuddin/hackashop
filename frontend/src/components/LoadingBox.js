f6f0658d831febea2a57a222e1ad0aec81f69e8b
Ameynuddin <md.ameynuddin@gmail.com>
2024-09-04 14:00:40 +0800

    updated loading

--- frontend/src/components/LoadingBox.js
+++ frontend/src/components/LoadingBox.js
@@ -6,8 +6,11 @@ export default function LoadingBox() {
             <Spinner animation="border" role="status">
                 <span className="visually-hidden">Loading...</span>
             </Spinner>
-            <span>Ensuring the best for your experience....</span>
-            <span>Please wait while the server loads as it is hosted on a free instance of Render which may take a moment to start up {':)'}</span>
+            <div className='text-center mt-3 text-wrap w-50 lh-base'>
+                <p>Ensuring the best for your experience....</p>
+                <p>Please wait while the server loads as it is hosted on a free instance of Render which may take a moment to start up {':)'}</p>
+            </div>
+
         </>
     );
 }
