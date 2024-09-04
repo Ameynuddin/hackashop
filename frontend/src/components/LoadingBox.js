9bbcd9e4a0ff5879e0dfd3374ce9b7f944120f09
Ameynuddin <md.ameynuddin@gmail.com>
2024-09-04 13:53:41 +0800

    edited loading box

--- frontend/src/components/LoadingBox.js
+++ frontend/src/components/LoadingBox.js
@@ -2,10 +2,12 @@ import Spinner from 'react-bootstrap/Spinner';
 
 export default function LoadingBox() {
     return (
-        <Spinner animation="border" role="status">
-            <span className="visually-hidden">Loading...</span>
+        <>
+            <Spinner animation="border" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </Spinner>
             <span>Ensuring the best for your experience....</span>
             <span>Please wait while the server loads as it is hosted on a free instance of Render which may take a moment to start up {':)'}</span>
-        </Spinner>
+        </>
     );
 }
